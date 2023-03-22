fc32c3b8a72e81529a91e8fee172ab60ee528052
8dcc <8dcc.git@gmail.com>
2023-03-22 16:15:57 +0100

    Add initial ncurses source

---
+++ src/defines.h
@@ -0,0 +1,13 @@
+
+#ifndef _DEFINES_H
+#define _DEFINES_H 1
+
+/* Special keys */
+#define KEY_UARROW 259
+#define KEY_DARROW 258
+#define KEY_LARROW 260
+#define KEY_RARROW 261
+#define KEY_CTRLC  3
+
+#endif /* _DEFINES_H */
+
--- src/main.c
+++ src/main.c
@@ -1,8 +1,53 @@
 
 #include <stdio.h>
+#include <ctype.h> /* tolower */
+#include <ncurses.h>
+
+#include "defines.h"
 
 int main(int argc, char** argv) {
-    printf("Hello, world!\n");
+    initscr();            /* Init ncurses */
+    raw();                /* Scan input without pressing enter */
+    noecho();             /* Don't print when typing */
+    keypad(stdscr, TRUE); /* Enable keypad (arrow keys) */
+
+    /* Char the user is pressing */
+    int c = 0;
+    do {
+        /* TODO: Do stuff */
+        mvprintw(0, 0, "Char: %c", c);
+
+        /* Wait for user input */
+        c = tolower(getch());
+
+        /* Parse input. 'q' quits and there is vim-like navigation */
+        switch (c) {
+            case 'k':
+            case KEY_UARROW:
+                /* TODO: Directions */
+                break;
+            case 'j':
+            case KEY_DARROW:
+                /* TODO: Directions */
+                break;
+            case 'h':
+            case KEY_LARROW:
+                /* TODO: Directions */
+                break;
+            case 'l':
+            case KEY_RARROW:
+                /* TODO: Directions */
+                break;
+            case KEY_CTRLC:
+                c = 'q';
+                break;
+            case 'q':
+            default:
+                break;
+        }
+    } while (c != 'q');
+
+    endwin();
     return 0;
 }
 
